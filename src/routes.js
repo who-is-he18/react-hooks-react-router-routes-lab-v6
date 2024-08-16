74e40570f4ba4ccf4d37a79ceeb3fbe181175ed4
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-08-16 18:59:55 +0300

    lab

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ import Actors from "./pages/Actors";
 import Movie from "./pages/Movie";
 import ErrorPage from "./pages/ErrorPage";
 
+
+
+// function Routes
+
 const routes = [
   { path: "/", element: <Home />, errorElement: <ErrorPage /> },
   { path: "/directors", element: <Directors />, errorElement: <ErrorPage /> },
@@ -12,5 +16,6 @@ const routes = [
   { path: "*", element: <ErrorPage /> },
 ];
 // export default routes;
+
 
 export default routes;
